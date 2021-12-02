00bdf55427c384d995e4c9735b958a564ccdca39
udinberak <naikmaster70@gmail.com>
2021-12-02 20:01:31 +0700

    bot

---
+++ shop/allpayment.js
@@ -0,0 +1,29 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY Hanif Bot
+
+*さ BANK*
+1. BRI 
+	ㅁ 5074-01-003141-52-8
+		• R***A*DI
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 6281 25276 6480
+		• ZE**** **C
+2. OVO
+	ㅁ 6281 25276 6480
+		• EL*****TI
+3. DANA
+	ㅁ 6281 25276 6480
+		• ZE**** **C
+
+*さ PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
